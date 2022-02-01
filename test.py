eccd3bbd146a101c244717f96eb5af9441096b8b
Fima20 <88209050+Fima20@users.noreply.github.com>
2022-02-01 19:47:02 +0500

    add test.py

--- test.py
+++ test.py
@@ -3,10 +3,8 @@ import datetime
 
 date_format = '%Y-%m-%d %H:%M:%S.%f'
 if __name__ == '__main__':
-    print("main.py")
-
+    print("test.py")
     config = uniconf.Config()
     # config.set("info_4", "number", "125721", "int")
-    a_bool = True
     config.struct()
 
